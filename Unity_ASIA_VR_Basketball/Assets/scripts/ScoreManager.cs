00d6e38fe4ddd8cb86f8f89b22d265aa7a20611a
ououopel0001 <71679877+ououopel0001@users.noreply.github.com>
2020-12-29 11:49:49 +0800

    進球

--- Unity_ASIA_VR_Basketball/Assets/scripts/ScoreManager.cs
+++ Unity_ASIA_VR_Basketball/Assets/scripts/ScoreManager.cs
@@ -7,9 +7,48 @@ public class ScoreManager : MonoBehaviour
     public Text textScore;
     [Header("分數")]
     public int score;
+    [Header("投進的分數")]
+    public int scoreIn = 2;
+    [Header("進球音效")]
+    public AudioClip soundIn;
+
+    private AudioSource aud;
+
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        print(other.name);
+        if(other.tag=="籃球")
+        {
+            AddScore();
+        }
+
+        if (other.transform.root.name == "Player") 
+        {
+            scoreIn = 3;
+        }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.root.name == "Player") 
+        {
+            scoreIn = 2;
+        }
+    }
+
+    private void AddScore()
+    {
+        score += scoreIn;
+        textScore.text = "分數:" + score;
+        aud.PlayOneShot(soundIn, Random.Range(1f,2f));
+
+    }
+
+
+
+
 }
